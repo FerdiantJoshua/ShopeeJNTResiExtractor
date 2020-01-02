f94d40201b4dac9344f7f26f0c0b7e687524fb4e
FerdiantJoshua <ferdiant.98@gmail.com>
2020-01-02 13:23:40 +0700

    Finish resi_shopee_int_extractor

--- resi_shopee_jnt_extractor.py
+++ resi_shopee_jnt_extractor.py
@@ -1,7 +1,8 @@
 import csv
 import os
+import re
 
-from bs4 import BeautifulSoup
+import fitz
 
 
 DEFAULT_INPUT_DIR = 'input'
@@ -9,34 +10,52 @@ DEFAULT_OUTPUT_DIR = 'output'
 DEFAULT_OUTPUT_NAME = 'data_resi.csv'
 COLUMNS = ['Nama', 'Kota', 'Biaya', 'Resi']
 
-def is_div(name: str) -> bool:
-    return name == 'div'
-
 def parse_resi_data_from_file(filename: str) -> list:
-    with open(filename, 'r') as f_in:
-        soup = BeautifulSoup(f_in, 'lxml')
+    texts = []
+    doc = fitz.open(filename)
+    for page in doc:
+        text = page.getText()
+        texts.append(text.split('\n'))
 
-    complete_resi_data = []
+    kode_resi_regex = r'([A-Z]{3}-)?([A-Z]{3})[0-9]{3}'
+    kode_pos_regex = r'[0-9]{5}'
 
-    i = 0
-    for object in soup.body.div.contents:
-        if not is_div(object.name) or object.div is None:
-            continue
-        # print(i)
-        i += 1
-        resi = object.div
-        header = resi.div
-        barcode_text = header.find_all('div', class_='barcode-text')[0].string
-        content = header.next_sibling.next_sibling
-        data = content.find_all('div', class_=['data-name', 'data-address-city', 'data-total-fee'])
-        name = data[0].contents[0]
-        city = data[1].string
-        total_fee = data[3].string.replace('Total Biaya: Rp', '').replace('.','')
-        complete_resi_data.append([name, city, total_fee, barcode_text])
-        # print(complete_resi_data)
-        # print()
-
-    return complete_resi_data
+    data = []
+    for text in texts:
+        nama_countdown = 0
+        kota_countdown = 0
+        harga_countdown = 0
+        resi_countdown = 0
+        datum = []
+        for i in range(len(text)):
+            nama_countdown -= 1
+            kota_countdown -= 1
+            harga_countdown -= 1
+            resi_countdown -= 1
+            if nama_countdown == 0:
+                datum.append(text[i].strip())
+            elif kota_countdown == 0:
+                j = 0
+                kota = ''
+                while not re.match(kode_pos_regex, text[i+j]):
+                    kota += f' {text[i+j]}'
+                    j += 1
+                kota = kota.split(',')[-2].strip()
+                datum.append(kota)
+            elif harga_countdown == 0:
+                datum.append(text[i].split('Rp ')[-1].strip())
+            elif resi_countdown == 0:
+                datum.append(text[i].strip())
+                data.append(datum.copy())
+                datum = []
+            elif re.match(kode_resi_regex, text[i]):
+                nama_countdown = 1
+                kota_countdown = 3
+            elif text[i] == 'Biaya':
+                harga_countdown = 1
+            elif text[i] == 'Daftar Produk':
+                resi_countdown = 7
+    return data
 
 def write_to_csv(data: list, output_path: str, prefix: str, filename: str) -> None:
     filename = filename.split('.')[0] + '.csv'
@@ -52,7 +71,7 @@ def main():
     output_path = DEFAULT_OUTPUT_DIR
 
     count = 0
-    for filename in os.listdir(input_path):
+    for filename in os.walk(input_path).__next__()[-1]:
         file_ = input_path + '/' + filename
         try:
             print('Parsing {}...'.format(file_))
--- tes.py
+++ tes.py
@@ -1,15 +1,59 @@
-import PyPDF2
+import copy
+import re
+import sys
 
+import fitz
 
-with open('input/download.pdf', 'rb') as f_in:    
-    # pdf reader object
-    pdfReader = PyPDF2.PdfFileReader(f_in)
-    # number of pages in pdf
-    print(pdfReader.documentInfo)
-    print(pdfReader.numPages)
-    # a page object
-    pageObj = pdfReader.getPage(0)
-    # extracting text from page.
-    # this will print the text you can also save that into String
-    print(pageObj.extractText())
+filename = sys.argv[1]
 
+texts = []
+doc = fitz.open(f'input/{filename}')
+for page in doc:
+    text = page.getText()
+    texts.append(text.split('\n'))
+
+kode_resi_regex = r'([A-Z]{3}-)?([A-Z]{3})[0-9]{3}'
+kode_pos_regex = r'[0-9]{5}'
+
+data = []
+for text in texts:
+    nama_countdown = 0
+    kota_countdown = 0
+    harga_countdown = 0
+    resi_countdown = 0
+    datum = []
+    for i in range(len(text)):
+        nama_countdown -= 1
+        kota_countdown -= 1
+        harga_countdown -= 1
+        resi_countdown -= 1
+        if nama_countdown == 0:
+            datum.append(text[i].strip())
+        elif kota_countdown == 0:
+            j = 0
+            kota = ''
+            while not re.match(kode_pos_regex, text[i+j]):
+                kota += f' {text[i+j]}'
+                j += 1
+            kota = kota.split(',')[-2].strip()
+            datum.append(kota)
+        elif harga_countdown == 0:
+            datum.append(text[i].split('Rp ')[-1].strip())
+        elif resi_countdown == 0:
+            datum.append(text[i].strip())
+            data.append(datum.copy())
+            datum = []
+        elif re.match(kode_resi_regex, text[i]):
+            nama_countdown = 1
+            kota_countdown = 3
+        elif text[i] == 'Biaya':
+            harga_countdown = 1
+        elif text[i] == 'Daftar Produk':
+            resi_countdown = 7
+
+print(data)
+
+with open('output/test.txt', 'w') as f_out:
+    for text in texts:
+        for line in text:
+            f_out.writelines(f'{line}\n')
